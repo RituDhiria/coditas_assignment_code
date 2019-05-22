d64ef9cb6722d024064afd5255c4ca806aad8137
hp <ritud1990@gmail.com>
2019-05-23 03:49:58 +0530

    message

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { SearchService } from './search.service';
+
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [SearchService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+
